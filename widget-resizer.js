aa05d38cc040253c76f532956df168097a391fcc
CubeZockii <169075971+CubeZockii@users.noreply.github.com>
2025-09-14 22:11:35 +0200

    Update widget-resizer.js

--- widget-resizer.js
+++ widget-resizer.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const body = document.body;
         const height = body.scrollHeight;
         // Increase the width by a fixed amount, e.g., 50 pixels
-        const width = body.scrollWidth + 25; 
+        const width = body.scrollWidth + 20; 
 
         window.parent.postMessage({
             type: 'resize-widget',
